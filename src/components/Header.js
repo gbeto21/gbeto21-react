f503144c0209eef2070dab5b91f7ffef9dfd2d61
Alberto González <albertoglezb@hotmail.com>
2021-12-02 09:35:05 -0600

    The animated icons were added.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,35 @@
 import React from 'react'
 import Typed from "react-typed";
+import { motion } from "framer-motion"
 
 const Header = () => {
+
+    const container = {
+        hidden: {
+            opacity: 1,
+            scale: 0
+        },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transsition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2
+            }
+        }
+    }
+
+    const item = {
+        hidden: {
+            y: 30,
+            opacity: 0
+        },
+        visible: {
+            y: 0,
+            opacity: 1
+        }
+    }
+
     return (
         <div className="header-wraper">
             <div className="main-info">
@@ -15,6 +43,19 @@ const Header = () => {
                     backSpeed={60}
                     loop
                 />
+                <motion.div
+                    className="icons-wrapper"
+                    variants={container}
+                    initial='hidden'
+                    animate='visible'
+                >
+                    <div style={{ display: 'flex' }}>
+                        <motion.div className="item-1 item" variants={item}></motion.div>
+                        <motion.div className="item-2 item" variants={item}></motion.div>
+                        <motion.div className="item-3 item" variants={item}></motion.div>
+                        <motion.div className="item-4 item" variants={item}></motion.div>
+                    </div>
+                </motion.div>
             </div>
         </div>
     )
