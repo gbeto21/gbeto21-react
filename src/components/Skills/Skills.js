d0baf10fa1321bf145d0c6889ea74dc375eded62
Alberto González <albertoglezb@hotmail.com>
2021-12-02 20:02:36 -0600

    A initial tab for the skills section was created.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Skills.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
 const Skills = () => {
 
     return (
         <div>
-            <h1>Hello skills.</h1>
+            <div style={{ display: 'block', width: 700, padding: 30 }}>
+                <Tabs defaultActiveKey="first">
+                    <Tab eventKey="first" title="Dashboard">
+                        Hii, I am 1st tab content
+                    </Tab>
+                    <Tab eventKey="second" title="Setting">
+                        Hii, I am 2nd tab content
+                    </Tab>
+                    <Tab eventKey="third" title="Aboutus">
+                        Hii, I am 3rd tab content
+                    </Tab>
+                </Tabs>
+            </div>
         </div>
     )
 }
