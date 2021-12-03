cc2dd2c1ef7c815ba037204e713b1a2025694b35
Alberto González <albertoglezb@hotmail.com>
2021-12-03 17:05:03 -0600

    The SwiperTech component was implemented.

--- src/components/SwiperTech/SwiperTech.js
+++ src/components/SwiperTech/SwiperTech.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination } from "swiper/core";
+import SwiperCore, { Pagination, Autoplay, Navigation } from "swiper/core";
 import "swiper/swiper.min.css";
 import "swiper/components/effect-coverflow/effect-coverflow.min.css";
 import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/navigation/navigation.min.css";
 import "./SwiperTech.css";
 
-SwiperCore.use([Pagination]);
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 const SwiperTech = () => {
   return (
@@ -22,19 +23,38 @@ const SwiperTech = () => {
         modifier: 6,
         slideShadows: true,
       }}
-      pagination={true}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+      }}
+      loop={true}
     >
       <SwiperSlide>
-        <div className="item-1 item"></div>
+        <div className="item item-gh"></div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="item-1 item"></div>
+        <div className="item item-js"></div>
+      </SwiperSlide>
+      <SwiperSlide className="slide-nd">
+        <div className="item item-nd"></div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="item-1 item"></div>
+        <div className="item item-ex"></div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="item-1 item"></div>
+        <div className="item item-rt"></div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="item item-ts"></div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="item item-aw"></div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="item item-sl"></div>
+      </SwiperSlide>
+      <SwiperSlide className="slide-cs">
+        <div className="item item-cs"></div>
       </SwiperSlide>
     </Swiper>
   );
