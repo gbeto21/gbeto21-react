dcb19f44178b9732f296d9e8abcf2fdb77c158cc
Alberto González <albertoglezb@hotmail.com>
2021-12-02 20:25:25 -0600

    A initial set up for the portfolio section was created.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
 
 const Portfolio = () => {
+
+    const optionsArray = [
+        { key: "au", label: "Australia" },
+        { key: "ca", label: "Canada" },
+        { key: "us", label: "USA" },
+        { key: "pl", label: "Poland" },
+        { key: "es", label: "Spain" },
+        { key: "fr", label: "France" },
+    ];
+
+    const onChange = selected => {
+        console.log(selected);
+    }
+
     return (
         <div>
-            <h1>Hello from Portfolio.</h1>
+            <DropdownMultiselect handleOnChange={onChange} options={optionsArray} name="countries" />
+            <button type="button" class="btn btn-outline-success">Search</button>
         </div>
     )
 }
