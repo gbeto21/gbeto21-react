56095a49843540bf7c2be3ddf591d3500a92daaa
Alberto González <albertoglezb@hotmail.com>
2021-12-04 08:04:14 -0600

    The green color was implemented.

--- src/components/SocialNetwork/SocialNetwork.js
+++ src/components/SocialNetwork/SocialNetwork.js
@@ -1,22 +1,22 @@
 import React from "react";
-import "./SocialNetwork.css"
+import "./SocialNetwork.css";
 
 const SocialNetwork = () => {
   return (
     <div className="social-network-container">
-      <a href="https://github.com/gbeto21">
+      <a href="https://github.com/gbeto21" target="_blank">
         <i className="fa fa-github-square"></i>
       </a>
-      <a href="https://mx.linkedin.com/in/gbeto21">
+      <a href="https://mx.linkedin.com/in/gbeto21" target="_blank">
         <i className="fa fa-linkedin-square"></i>
       </a>
-      <a href="https://twitter.com/gbeto21">
+      <a href="https://twitter.com/gbeto21" target="_blank">
         <i className="fa fa-twitter"></i>
       </a>
-      <a href="https://www.instagram.com/gbeto21/">
+      <a href="https://www.instagram.com/gbeto21/" target="_blank">
         <i className="fa fa-instagram"></i>
       </a>
-      <a href="https://www.facebook.com/gbeto21">
+      <a href="https://www.facebook.com/gbeto21" target="_blank">
         <i className="fa fa-facebook-square"></i>
       </a>
     </div>
