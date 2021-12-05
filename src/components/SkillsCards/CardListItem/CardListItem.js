506f6dea4c390712be027d0f8cfb31fcbebc9669
Alberto González <albertoglezb@hotmail.com>
2021-12-05 13:06:30 -0600

    The languages card was completed.

--- src/components/SkillsCards/CardListItem/CardListItem.js
+++ src/components/SkillsCards/CardListItem/CardListItem.js
@@ -1,15 +1,20 @@
 import React from "react";
+import "./CardListItem.css";
 
 const CardListItem = ({ item }) => {
   return (
     <li className="list-group-item">
       <div className="row g-0">
-        <div className="col-md-4">
-          <img src={item.image} className="img-fluid" alt={item.name} />
+        <div className="col-2 container-center">
+          <img
+            src={item.image}
+            className={`img-fluid ${item.classCSS}`}
+            alt={item.name}
+          />
         </div>
-        <div className="col-md-8">
+        <div className="col-10">
           <div className="card-body">
-            <h5 className="card-title">{item.name}</h5>
+            <h6 className="card-title">{item.name}</h6>
             <p className="card-text">
               <small className="text-muted">{item.level}</small>
             </p>
