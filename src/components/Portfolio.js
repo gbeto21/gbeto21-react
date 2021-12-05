71773b076c1415a8d320686b41f878b1e8a08122
Alberto González <albertoglezb@hotmail.com>
2021-12-05 16:40:29 -0600

    Styles were added in the Portfolio page.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React from "react";
 import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
 
 const Portfolio = () => {
+  const optionsArray = [
+    { key: "au", label: "Australia" },
+    { key: "ca", label: "Canada" },
+    { key: "us", label: "USA" },
+    { key: "pl", label: "Poland" },
+    { key: "es", label: "Spain" },
+    { key: "fr", label: "France" },
+  ];
 
-    const optionsArray = [
-        { key: "au", label: "Australia" },
-        { key: "ca", label: "Canada" },
-        { key: "us", label: "USA" },
-        { key: "pl", label: "Poland" },
-        { key: "es", label: "Spain" },
-        { key: "fr", label: "France" },
-    ];
+  const onChange = (selected) => {
+    console.log(selected);
+  };
 
-    const onChange = selected => {
-        console.log(selected);
-    }
-
-    return (
-        <div>
-            <DropdownMultiselect handleOnChange={onChange} options={optionsArray} name="countries" />
-            <button type="button" className="btn btn-outline-success">Search</button>
+  return (
+    <div className="container-background-start">
+      <div className="container container-skills">
+        <div className="row row-cols-2">
+          <div className="col-10">
+            <DropdownMultiselect
+              handleOnChange={onChange}
+              options={optionsArray}
+              name="countries"
+            />
+          </div>
+          <div className="col-2">
+            <button type="button" className="btn btn-outline-success">
+              Search
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
