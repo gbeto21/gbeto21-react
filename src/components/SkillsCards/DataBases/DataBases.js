e22cabd9e9f98446ae0885f843d6a58204d9ad33
Alberto González <albertoglezb@hotmail.com>
2021-12-05 13:40:02 -0600

    The databases card was completed.

--- src/components/SkillsCards/DataBases/DataBases.js
+++ src/components/SkillsCards/DataBases/DataBases.js
@@ -1,17 +1,9 @@
 import React from "react";
+import BaseCard from "../BaseCard/BaseCard";
+import databases from "./dataBasesData.json";
 
 const CardDataBases = () => {
-  return (
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">DataBases</h5>
-        <p className="card-text">
-          This is a longer card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
-      </div>
-    </div>
-  );
+  return <BaseCard data={databases.data} title="Data Bases" />;
 };
 
 export default CardDataBases;
