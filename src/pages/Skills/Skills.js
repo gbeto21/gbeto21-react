561a98b4956a07791be61bf5334dcc69e988778c
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-06 21:13:14 -0600

    The Appear animation was implemented in the Skills component.

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Skills.css";
+import AnimationAppear from "../../animations/Appear";
 import "bootstrap/dist/css/bootstrap.css";
 import CardLanguages from "../../components/SkillsCards/Languages/Languages";
 import CardFrameworks from "../../components/SkillsCards/Frameworks/Frameworks";
@@ -9,27 +10,29 @@ import CardCertifications from "../../components/SkillsCards/Certifications/Cert
 
 const Skills = () => {
   return (
-    <div className="container-background-start mb-3">
-      <div className="container container-skills">
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-4 g-4">
-          <div className="col">
-            <CardLanguages />
-          </div>
-          <div className="col">
-            <CardFrameworks />
-          </div>
-          <div className="col">
-            <CardDataBases />
-          </div>
-          <div className="col">
-            <CardDevOps />
-          </div>
-          <div className="col mb-3">
-            <CardCertifications />
+    <AnimationAppear>
+      <div className="container-background-start mb-3">
+        <div className="container container-skills">
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-4 g-4">
+            <div className="col">
+              <CardLanguages />
+            </div>
+            <div className="col">
+              <CardFrameworks />
+            </div>
+            <div className="col">
+              <CardDataBases />
+            </div>
+            <div className="col">
+              <CardDevOps />
+            </div>
+            <div className="col mb-3">
+              <CardCertifications />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </AnimationAppear>
   );
 };
 
