69e0898b02f12d4c1767def8e4eeef7f5db330f1
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-06 19:01:43 -0600

    The waves home component was created.

---
+++ src/components/Waves/Home/WavesHome.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const WavesHome = () => {
+  return (
+    <div className="container-waves">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#0099ff"
+          fill-opacity="0.22"
+          d="M0,256L60,266.7C120,277,240,299,360,272C480,245,600,171,720,160C840,149,960,203,1080,202.7C1200,203,1320,149,1380,122.7L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export default WavesHome;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Greetings from "../../components/Greetings/Greetings";
 import { motion } from "framer-motion";
 import "./Home.css";
 import SwiperTech from "../../components/SwiperTech/SwiperTech";
+import WavesHome from "../../components/Waves/Home/WavesHome";
 
 const Home = () => {
   const container = {
@@ -46,15 +47,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="container-waves">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path
-            fill="#0099ff"
-            fill-opacity="0.22"
-            d="M0,256L60,266.7C120,277,240,299,360,272C480,245,600,171,720,160C840,149,960,203,1080,202.7C1200,203,1320,149,1380,122.7L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
-          ></path>
-        </svg>
-      </div>
+      <WavesHome />
     </Fragment>
   );
 };
