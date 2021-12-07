6f4e4d8ebfad49fcc9c706c1b1789e3369704dd1
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-06 21:11:08 -0600

    The Appear animation was implemented in the About component.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import AnimationAppear from "../../animations/Appear";
 import Separator from "../../components/Separator/Separator";
 import SocialNetwork from "../../components/SocialNetwork/SocialNetwork";
 import ProfilePicture from "../../components/ProfilePicture/ProfilePicture";
@@ -8,25 +9,27 @@ import Introduction from "../../components/Introduction/Introduction";
 const About = () => {
   return (
     <Fragment>
-      <div className="container-background-start">
-        <div className="container container-about">
-          <div className="container-profile-picture">
-            <ProfilePicture />
-          </div>
-          <div className="row justify-content-md-center">
-            <SocialNetwork />
-          </div>
-          <div className="row justify-content-center">
-            <Separator />
-          </div>
-          <div className="row justify-content-center">
-            <Introduction />
-          </div>
-          <div className="row justify-content-center">
-            <PersonalInfo />
+      <AnimationAppear>
+        <div className="container-background-start">
+          <div className="container container-about">
+            <div className="container-profile-picture">
+              <ProfilePicture />
+            </div>
+            <div className="row justify-content-md-center">
+              <SocialNetwork />
+            </div>
+            <div className="row justify-content-center">
+              <Separator />
+            </div>
+            <div className="row justify-content-center">
+              <Introduction />
+            </div>
+            <div className="row justify-content-center">
+              <PersonalInfo />
+            </div>
           </div>
         </div>
-      </div>
+      </AnimationAppear>
     </Fragment>
   );
 };
