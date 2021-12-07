3cca47afe333017bf59b92b0db6d9542f172db1c
Alberto González Balderas <albertoglezb@hotmail.com>
2021-12-06 21:07:04 -0600

    The Home import was moved in the Home.js component.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react";
+import "./Home.css";
 import Greetings from "../../components/Greetings/Greetings";
 import SwiperTech from "../../components/SwiperTech/SwiperTech";
 import WavesHome from "../../components/Waves/Home/WavesHome";
-import "./Home.css";
 
 const Home = () => {
   return (
